44fab1a817b426373c4d407b30b27a4d480c1e11
bwael <bwael@foxmail.com>
2016-10-20 21:32:18 +0800

    add Constant.java 常量类

---
+++ src/cn/xunsci/simplepush/node/Constant.java
@@ -0,0 +1,13 @@
+package cn.xunsci.simplepush.node;
+
+public class Constant {
+
+	//final
+	public static final int CLIENT_MESSAGE_MIN_LENGTH = 21;
+	public static final int CLIENT_MESSAGE_MAX_LENGTH = 29;
+	
+	public static final int SERVER_MESSAGE_MIN_LENGTH = 5;
+	public static final int PUSH_MSG_HEADER_LEN = 21;
+	
+	public static int VERSION_NUM = 1;
+}
